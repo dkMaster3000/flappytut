4269e22dc9f9d93f79a7370b38d1dd3e8ca700e3
dkMaster3000 <kunzd007@gmail.com>
2023-10-21 20:44:44 +0200

    birddead

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
-    public float flapStrength;
+    public float flapStrength = 10;
 
     public LogicScript logic;
     public bool birdIsAlive = true;
+
+    public float deadZoneTop = 15;
+    public float deadZoneBottom = -15;
 
 
 
@@ -26,13 +29,26 @@ public class BirdScript : MonoBehaviour
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+     
+        if (transform.position.y < deadZoneBottom || transform.position.y > deadZoneTop)
+        {
+            birdDead();
+        }
+
+        if (birdIsAlive == false)
+        {
+            logic.gameOver();
+        }
         
-        
+    }
+
+   void birdDead()
+    {
+        birdIsAlive = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        logic.gameOver();
-        birdIsAlive = false;
+        birdDead();
     }
 }
